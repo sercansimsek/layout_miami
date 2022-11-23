6152dfbaa4b5f18440b1272209a49dc1d7620d10
Sercan Simsek <sercansimsek44@gmail.com>
2022-11-23 12:41:23 +0100

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--disable-scroll');
-  } else {
-    document.body.classList.remove('page__body--disable-scroll');
-  }
-});
